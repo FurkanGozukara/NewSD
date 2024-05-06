12935c83defa0ba087bd78fc37fff0d95fac69f8
Furkan Gözükara <furkangozukara@gmail.com>
2024-05-06 06:12:42 +0300

    v9

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 import gc
 import os
 import random
+import sys
+import platform
+sys.path.append("./")
 import gradio as gr
 import numpy as np
 import torch
+from PIL import Image, PngImagePlugin
+import datetime
 
 from pipelines.pipeline_common import quantize_4bit, torch_gc
 from pipelines.pipeline_stable_cascade import StableCascadeDecoderPipelineV2
@@ -11,9 +16,7 @@ from pipelines.pipeline_stable_cascade_prior import StableCascadePriorPipelineV2
 from models.unets.unet_stable_cascade import StableCascadeUNet
 from diffusers.utils import logging
 
-import os
-import datetime
-import argparse  # Import the argparse library
+import argparse
 
 # Set up argument parser
 parser = argparse.ArgumentParser(description="Gradio interface for text-to-image generation with optional features.")
@@ -30,8 +33,8 @@ args = parser.parse_args()
 share = args.share
 
 load_mode = args.load_mode
-ENABLE_CPU_OFFLOAD = args.lowvram  # Use the offload argument to toggle ENABLE_CPU_OFFLOAD
-USE_TORCH_COMPILE = args.torch_compile  # Use the offload argument to toggle ENABLE_CPU_OFFLOAD
+ENABLE_CPU_OFFLOAD = args.lowvram
+USE_TORCH_COMPILE = args.torch_compile
 torch.backends.cudnn.allow_tf32 = False
 torch.backends.cuda.allow_tf32 = False
 need_restart_cpu_offloading = False
@@ -39,16 +42,16 @@ need_restart_cpu_offloading = False
 dtype = torch.bfloat16 if torch.cuda.is_bf16_supported() else torch.float16
 
 dtypeQuantize = dtype
-if(load_mode in ('8bit', '4bit')):
+if load_mode in ('8bit', '4bit'):
     dtypeQuantize = torch.float8_e4m3fn
 
 lite = "_lite" if args.lite else ""
 
 print(f"used dtype {dtypeQuantize}")
 os.environ['TOKENIZERS_PARALLELISM'] = 'false'
-DESCRIPTION = "<p style=\"font-size:14px\">Stable Cascade Modified By SECourses - Unofficial demo for <a href='https://huggingface.co/stabilityai/stable-cascade' target='_blank'>Stable Casacade</a>, a new high resolution text-to-image model by Stability AI, built on the Würstchen architecture.<br/> Some tips: Higher batch size working great with fast speed and not much VRAM usage - Not all resolutions working e.g. 1920x1080 fails but 1920x1152 works<br/>Supports high resolutions very well such as 1536x1536</p>"
+
 if not torch.cuda.is_available():
-    DESCRIPTION += "<br/><p>Running on CPU 🥶</p>"
+    print("Running on CPU 🥶")
 
 MAX_SEED = np.iinfo(np.int32).max
 MAX_IMAGE_SIZE = 4096
@@ -63,9 +66,9 @@ pipe_decoder_unet = None
 decoder_pipeline = None
 
 def restart_cpu_offload():
-    if load_mode != '4bit' :
+    if load_mode != '4bit':
         prior_pipeline.disable_xformers_memory_efficient_attention()
-        decoder_pipeline.disable_xformers_memory_efficient_attention()               
+        decoder_pipeline.disable_xformers_memory_efficient_attention()
     from pipelines.pipeline_common import optionally_disable_offloading
     optionally_disable_offloading(prior_pipeline)
     optionally_disable_offloading(decoder_pipeline)
@@ -73,14 +76,66 @@ def restart_cpu_offload():
     torch.cuda.empty_cache()
     prior_pipeline.enable_model_cpu_offload()
     decoder_pipeline.enable_model_cpu_offload()
-    if load_mode != '4bit' :
+    if load_mode != '4bit':
         prior_pipeline.enable_xformers_memory_efficient_attention()
         decoder_pipeline.enable_xformers_memory_efficient_attention()
 
-def randomize_seed_fn(seed: int, randomize_seed: bool) -> int:
-    if randomize_seed:
-        seed = random.randint(0, MAX_SEED)
-    return seed
+def read_image_metadata(image_path):
+    if image_path is None or not os.path.exists(image_path):
+        return "File does not exist or path is None."
+    
+    
+    last_modified_timestamp = os.path.getmtime(image_path)
+
+    last_modified_date = datetime.datetime.fromtimestamp(last_modified_timestamp).strftime('%d %B %Y, %H:%M %p - UTC')
+    with Image.open(image_path) as img:
+        metadata = img.info
+        metadata_str = f"Last Modified Date: {last_modified_date}\n"
+        for key, value in metadata.items():
+            metadata_str += f"{key}: {value}\n"
+            
+    return metadata_str
+
+def save_image_with_metadata(image, filename, metadata):
+    meta_info = PngImagePlugin.PngInfo()
+    for key, value in metadata.items():
+        meta_info.add_text(key, str(value))
+    image.save(filename, "PNG", pnginfo=meta_info)
+
+def set_metadata_settings(image_path):
+    if image_path is None:
+        return (gr.update(),) * 11  # Return a tuple of 11 gr.update() calls
+    
+    with Image.open(image_path) as img:
+        metadata = img.info
+        prompt = metadata.get("Prompt", "")
+        negative_prompt = metadata.get("Negative Prompt", "")
+        seed = int(metadata.get("Seed", "0"))
+        width = int(metadata.get("Width", "1024"))
+        height = int(metadata.get("Height", "1024"))
+        prior_guidance_scale = float(metadata.get("Prior Guidance Scale", "4.0"))
+        decoder_guidance_scale = float(metadata.get("Decoder Guidance Scale", "0.0"))
+        prior_num_inference_steps = int(metadata.get("Prior Inference Steps", "30"))
+        decoder_num_inference_steps = int(metadata.get("Decoder Inference Steps", "20"))
+        batch_size_per_prompt = int(metadata.get("Batch Size", "1"))
+        number_of_images_per_prompt = int(metadata.get("Number Of Images To Generate", "1"))
+
+    # Construct the updates list with gr.update calls for each setting
+    updates = [
+        gr.update(value=prompt),
+        gr.update(value=negative_prompt),
+        gr.update(value=seed),
+        gr.update(value=width),
+        gr.update(value=height),
+        gr.update(value=prior_guidance_scale),
+        gr.update(value=decoder_guidance_scale),
+        gr.update(value=prior_num_inference_steps),
+        gr.update(value=decoder_num_inference_steps),
+        gr.update(value=batch_size_per_prompt),
+        gr.update(value=number_of_images_per_prompt)
+    ]
+    
+    return tuple(updates)
 
 def generate(
     prompt: str,
@@ -93,12 +148,13 @@ def generate(
     decoder_num_inference_steps: int = 12,
     decoder_guidance_scale: float = 0.0,
     batch_size_per_prompt: int = 2,
-    number_of_images_per_prompt: int = 1,  # New parameter
+    number_of_images_per_prompt: int = 1,
+    randomize_seed_ck: bool = False,
 ):
     global pipe_prior_unet, prior_pipeline, pipe_decoder_unet, decoder_pipeline, need_restart_cpu_offloading
     if torch.cuda.is_available():        
         need_restart_cpu_offloading = False
-        if prior_pipeline == None:
+        if prior_pipeline is None:
             pipe_prior_unet = StableCascadeUNet.from_pretrained(
             model_id, subfolder=fr"prior{lite}").to(device, dtypeQuantize)
             
@@ -122,7 +178,7 @@ def generate(
             if ENABLE_CPU_OFFLOAD:
                 need_restart_cpu_offloading =True
         torch_gc()
-        if decoder_pipeline == None:
+        if decoder_pipeline is None:
             pipe_decoder_unet = StableCascadeUNet.from_pretrained(
             model_decoder_id, subfolder=fr"decoder{lite}").to(device, dtypeQuantize)
                 
@@ -135,7 +191,7 @@ def generate(
                 'torch_dtype': dtype,
                 'decoder': pipe_decoder_unet,
             }        
-            decoder_pipeline = StableCascadeDecoderPipelineV2.from_pretrained(**pipeline_decoder_param,).to(device)
+            decoder_pipeline = StableCascadeDecoderPipelineV2.from_pretrained(**pipeline_decoder_param).to(device)
             
             if load_mode == '4bit':
                 if decoder_pipeline.text_encoder is not None:
@@ -160,12 +216,12 @@ def generate(
             prior_pipeline.prior = torch.compile(prior_pipeline.prior, mode="reduce-overhead", fullgraph=True)
             decoder_pipeline.decoder = torch.compile(decoder_pipeline.decoder, mode="max-autotune", fullgraph=True)
        
-       
         images = []  # Initialize an empty list to collect generated images
         original_seed = seed  # Store the original seed value
         for i in range(number_of_images_per_prompt):
-            if i > 0:  # Update seed for subsequent iterations
+            if randomize_seed_ck or i > 0:  # Update seed if randomize is checked or for subsequent images
                 seed = random.randint(0, MAX_SEED)
+
             generator = torch.Generator().manual_seed(seed)
             with torch.cuda.amp.autocast(dtype=dtype):
                 prior_output = prior_pipeline(
@@ -204,144 +260,100 @@ def generate(
                 # Generate timestamped filename
                 timestamp = datetime.datetime.now().strftime('%Y_%m_%d_%H_%M_%S_%f')
                 image_filename = f"{output_folder}/{timestamp}.png"
-                image.save(image_filename)
+                
+                # Prepare metadata
+                metadata = {
+                    "Prompt": prompt,
+                    "Negative Prompt": negative_prompt,
+                    "Seed": seed,
+                    "Width": width,
+                    "Height": height,
+                    "Prior Guidance Scale": prior_guidance_scale,
+                    "Decoder Guidance Scale": decoder_guidance_scale,
+                    "Prior Inference Steps": prior_num_inference_steps,
+                    "Decoder Inference Steps": decoder_num_inference_steps
+                }
+                
+                # Save image with metadata
+                save_image_with_metadata(image, image_filename, metadata)
+                
                 if torch.cuda.is_available():
                     torch.cuda.empty_cache()
 
         # Return the list of generated images
-        return images
+        return images, seed
     else:
         prior_pipeline = None
         decoder_pipeline = None
         return
 
+def open_folder():
+    open_folder_path = os.path.abspath("outputs")
+    if platform.system() == "Windows":
+        os.startfile(open_folder_path)
+    elif platform.system() == "Linux":
+        os.system(f'xdg-open "{open_folder_path}"')
 
 with gr.Blocks() as app:
-    gr.Markdown("## V8 Stable Cascade by SECourses : 1-Click Installers Latest Version On : https://www.patreon.com/posts/98410661")
-    gr.Markdown("[Stable Cascade](https://stability.ai/news/introducing-stable-cascade) is the latest model of Stability AI based on Würstchen architecture")
-    gr.Markdown("Stable Cascade is compatible with GPUs having as little as 5 GB of memory and can generate high-quality images at resolutions even at 1536x1536 pixels. It supports resolution adjustments in 128-pixel steps, e.g. 1024x1024 or 1152x1024 or 1152x896")
-    with gr.Row():
-        with gr.Column():
-            prompt = gr.Text(
-                label="Prompt",
-                placeholder="Enter your prompt",
-            )
-            run_button = gr.Button("Generate")
-            
-            # Advanced options now directly visible
-            negative_prompt = gr.Text(
-                label="Negative prompt",
-                placeholder="Enter a Negative Prompt",
-            )
+    gr.Markdown(""" ### Stable Cascade V9 by SECourses : 1-Click Installers Latest Version On : https://www.patreon.com/posts/98410661 
+    ### [Stable Cascade](https://stability.ai/news/introducing-stable-cascade) is the latest model of Stability AI based on Würstchen architecture 
+    ### Stable Cascade is compatible with GPUs having as little as 5 GB of memory and can generate high-quality images at resolutions even at 1536x1536 pixels. It supports resolution adjustments in 128-pixel steps, e.g. 1024x1024 or 1152x1024 or 1152x896""")
+    
+    with gr.Tab("Image Generation"):
+        with gr.Row():
+            with gr.Column():
+                # Main settings column
+                with gr.Row():
+                    prompt = gr.Text(label="Prompt", placeholder="Enter your prompt")
+                with gr.Row():
+                    negative_prompt = gr.Text(label="Negative prompt", placeholder="Enter a Negative Prompt")
+                with gr.Row():
+                    seed = gr.Slider(label="Seed", minimum=0, maximum=MAX_SEED, step=1, value=0)
+                    randomize_seed_ck = gr.Checkbox(label="Randomize seed", value=True)
+                with gr.Row():
+                    width = gr.Slider(label="Width", minimum=512, maximum=MAX_IMAGE_SIZE, step=128, value=1024)
+                    height = gr.Slider(label="Height", minimum=512, maximum=MAX_IMAGE_SIZE, step=128, value=1024)
+                with gr.Row():
+                    batch_size_per_prompt = gr.Slider(label="Batch Size", minimum=1, maximum=20, step=1, value=1)
+                    number_of_images_per_prompt = gr.Slider(label="Number Of Images To Generate", minimum=1, maximum=9999999, step=1, value=1)
+                with gr.Row():
+                    prior_guidance_scale = gr.Slider(label="Prior Guidance Scale (CFG)", minimum=0, maximum=20, step=0.1, value=4.0)
+                    decoder_guidance_scale = gr.Slider(label="Decoder Guidance Scale (CFG)", minimum=0, maximum=20, step=0.1, value=0.0)
+                with gr.Row():
+                    prior_num_inference_steps = gr.Slider(label="Prior Inference Steps", minimum=1, maximum=100, step=1, value=30)
+                    decoder_num_inference_steps = gr.Slider(label="Decoder Inference Steps", minimum=1, maximum=100, step=1, value=20)
+                with gr.Row():
+                    run_button = gr.Button("Generate")
+                with gr.Row():
+                    btn_open_outputs = gr.Button("Open Outputs Folder (Works on Windows & Desktop Linux)")
+                    btn_open_outputs.click(fn=open_folder)
+            with gr.Column():
+                # Output and additional settings column
+                result = gr.Gallery(label="Result", show_label=False, height=768)
 
-            seed = gr.Slider(
-                label="Seed",
-                minimum=0,
-                maximum=MAX_SEED,
-                step=1,
-                value=0,
-            )
-            randomize_seed = gr.Checkbox(label="Randomize seed", value=True)
-            with gr.Row():
-                with gr.Column():
-                    width = gr.Slider(
-                        label="Width",
-                        minimum=512,
-                        maximum=MAX_IMAGE_SIZE,
-                        step=128,
-                        value=1024,
-                    )
-                with gr.Column():
-                    height = gr.Slider(
-                        label="Height",
-                        minimum=512,
-                        maximum=MAX_IMAGE_SIZE,
-                        step=128,
-                        value=1024,
-                    )
-            with gr.Row():
-                with gr.Column():
-                    batch_size_per_prompt = gr.Slider(
-                        label="Batch Size",
-                        minimum=1,
-                        maximum=20,
-                        step=1,
-                        value=1,
-                    )
-                with gr.Column():
-                    number_of_images_per_prompt = gr.Slider(
-                        label="Number Of Images To Generate",
-                        minimum=1,
-                        maximum=9999999,
-                        step=1,
-                        value=1,
-                    )
-            with gr.Row():
-                with gr.Column():
-                    prior_guidance_scale = gr.Slider(
-                        label="Prior Guidance Scale (CFG)",
-                        minimum=0,
-                        maximum=20,
-                        step=0.1,
-                        value=4.0,
-                    )
-                with gr.Column():
-                    decoder_guidance_scale = gr.Slider(
-                        label="Decoder Guidance Scale (CFG)",
-                        minimum=0,
-                        maximum=20,
-                        step=0.1,
-                        value=0.0,
-                    )
-            with gr.Row():
-                with gr.Column():
-                    prior_num_inference_steps = gr.Slider(
-                        label="Prior Inference Steps",
-                        minimum=1,
-                        maximum=100,
-                        step=1,
-                        value=30,
-                    )
-                with gr.Column():
-                    decoder_num_inference_steps = gr.Slider(
-                        label="Decoder Inference Steps",
-                        minimum=1,
-                        maximum=100,
-                        step=1,
-                        value=20,
-                    )
-            
-        with gr.Column():
-            result = gr.Gallery(label="Result", show_label=False, height=768)
+        run_button.click(fn=generate, inputs=[
+            prompt, negative_prompt, seed, width, height,
+            prior_num_inference_steps, prior_guidance_scale,
+            decoder_num_inference_steps, decoder_guidance_scale,
+            batch_size_per_prompt, number_of_images_per_prompt, randomize_seed_ck
+        ], outputs=[result, seed])
 
-    inputs = [
-            prompt,
-            negative_prompt,
-            seed,
-            width,
-            height,
-            prior_num_inference_steps,
-            # prior_timesteps,
+    with gr.Tab("Image Metadata"):
+        with gr.Row():
+            set_metadata_button = gr.Button("Load & Set Metadata Settings")
+        with gr.Row():
+            with gr.Column():
+                metadata_image_input = gr.Image(type="filepath", label="Upload Image")
+            with gr.Column():
+                metadata_output = gr.Textbox(label="Image Metadata", lines=25, max_lines=50)
+
+        metadata_image_input.change(fn=read_image_metadata, inputs=[metadata_image_input], outputs=[metadata_output])
+        set_metadata_button.click(fn=set_metadata_settings, inputs=[metadata_image_input], outputs=[
+            prompt, negative_prompt, seed, width, height,
             prior_guidance_scale,
-            decoder_num_inference_steps,
-            # decoder_timesteps,
-            decoder_guidance_scale,
-            batch_size_per_prompt,
-            number_of_images_per_prompt
-    ]
-    gr.on(
-        triggers=[prompt.submit, negative_prompt.submit, run_button.click],
-        fn=randomize_seed_fn,
-        inputs=[seed, randomize_seed],
-        outputs=seed,
-        queue=False,
-        api_name=False,
-    ).then(
-        fn=generate,
-        inputs=inputs,
-        outputs=result,
-        api_name="run",
-    )
-		
+             decoder_guidance_scale, prior_num_inference_steps, decoder_num_inference_steps,
+            batch_size_per_prompt, number_of_images_per_prompt,
+        ])
+
 if __name__ == "__main__":
-    app.queue().launch(share=share,inbrowser=True)
+    app.launch(share=share, inbrowser=True)
